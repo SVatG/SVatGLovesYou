9efaf482e1df33777c4f1077f93e76628f524ab4
halcy <lorenzd@gmail.com>
2012-08-08 17:56:37 +0200

    Stupid hblank tricks.

--- effect3.c
+++ effect3.c
@@ -11,6 +11,10 @@
 #define DIR_LEFT 2
 #define DIR_DOWN 3
 
+#define RIBBON_COUNT 4
+#define MAX_SEGMENTS 512
+#define MAX_PERROW 60
+
 u16* ribbonSprite[4][4];
 u16* zeroSprite[4];
 
@@ -22,7 +26,7 @@ typedef struct ribbon_tile {
 } ribbon_tile;
 
 typedef struct ribbon {
-	ribbon_tile tiles[512];
+	ribbon_tile tiles[MAX_SEGMENTS];
 	int tile_count;
 	int head_status;
 	int head_dir;
@@ -30,28 +34,116 @@ typedef struct ribbon {
 	int y;
 } ribbon;
 
-ribbon ribbons[1];
+ribbon ribbons[RIBBON_COUNT];
 
 typedef struct row_tile {
 	int x;
 	int y;
 	u16* sprite;
-	int beforeHead;
+	int prio;
 } row_tile;
 
-row_tile row_tiles[12][124];
+row_tile row_tiles[12][MAX_PERROW];
+int row_tile_counts[12];
+
+uint16_t* palram = PALRAM_A;
+void switchSprites() {
+	int row = ((VCOUNT+16)>>4)&0xF;
+	int line = (VCOUNT)&0xF;
+	if(line <= 13) {
+		for(int i = line*4; i < line*4+4; i++) {
+			oamSet(
+				&oamMain, RIBBON_COUNT + i + 66*(row&0x1),
+				row_tiles[row][i].x,row_tiles[row][i].y,
+				row_tiles[row][i].prio, 0,
+				SpriteSize_16x16,
+				SpriteColorFormat_256Color,
+				row_tiles[row][i].sprite,
+				32, false, false, false, false, false
+			);
+		}
+	}
+	if(line == 13) {
+		DC_FlushRange(oamMain.oamMemory + 64*(row&0x1), sizeof(SpriteEntry)*64);
+		dmaCopy(oamMain.oamMemory + 64*(row&0x1), OAM, sizeof(SpriteEntry)*64);
+	}
+}
+
+void reset_rows() {
+	// Reset counts.
+	for(int i = 0; i < 12; i++) {
+		row_tile_counts[i] = 0;
+	}
+
+	// Reset positions
+	for(int i = 0; i < 12; i++) {
+		for(int j = 0; j < MAX_PERROW; j++) {
+			row_tiles[i][j].x = -16;
+			row_tiles[i][j].y = -16;
+			row_tiles[i][j].sprite = ribbonSprite[DIR_RIGHT][DIR_UP];
+			row_tiles[i][j].prio = 0;
+		}
+	}
+}
+
+void reset_ribbons() {
+	// Base ribbons.
+	ribbons[0].tiles[0].x = 8;
+	ribbons[0].tiles[0].y = 6;
+	ribbons[0].tiles[0].from = DIR_UP;
+	ribbons[0].tiles[0].to = DIR_DOWN;
+
+	ribbons[0].tile_count = 1;
+	ribbons[0].x = 8;
+	ribbons[0].y = 6;
+	ribbons[0].head_dir = DIR_DOWN;
+	ribbons[0].head_status = 3;
+
+	ribbons[1].tiles[0].x = 8;
+	ribbons[1].tiles[0].y = 6;
+	ribbons[1].tiles[0].from = DIR_LEFT;
+	ribbons[1].tiles[0].to = DIR_RIGHT;
+
+	ribbons[1].tile_count = 1;
+	ribbons[1].x = 8;
+	ribbons[1].y = 6;
+	ribbons[1].head_dir = DIR_RIGHT;
+	ribbons[1].head_status = 3;
+
+	ribbons[2].tiles[0].x = 8;
+	ribbons[2].tiles[0].y = 6;
+	ribbons[2].tiles[0].from = DIR_DOWN;
+	ribbons[2].tiles[0].to = DIR_UP;
+
+	ribbons[2].tile_count = 1;
+	ribbons[2].x = 8;
+	ribbons[2].y = 6;
+	ribbons[2].head_dir = DIR_UP;
+	ribbons[2].head_status = 3;
+
+	ribbons[3].tiles[0].x = 8;
+	ribbons[3].tiles[0].y = 6;
+	ribbons[3].tiles[0].from = DIR_RIGHT;
+	ribbons[3].tiles[0].to = DIR_LEFT;
+
+	ribbons[3].tile_count = 1;
+	ribbons[3].x = 8;
+	ribbons[3].y = 6;
+	ribbons[3].head_dir = DIR_LEFT;
+	ribbons[3].head_status = 3;
+}
 
 void effect3_init() {
 	
-	DISPCNT_A = DISPCNT_MODE_5 | DISPCNT_OBJ_ON | DISPCNT_ON;
+	DISPCNT_A = DISPCNT_MODE_5 | DISPCNT_OBJ_ON | DISPCNT_BG2_ON | DISPCNT_ON;
 
-	VRAMCNT_A = VRAMCNT_A_BG_VRAM_A_OFFS_0K;
-	BG2CNT_A = BGxCNT_EXTENDED_BITMAP_16 | BGxCNT_OVERFLOW_WRAP | BGxCNT_BITMAP_SIZE_256x256 | BGxCNT_BITMAP_BASE_0K;
+	VRAMCNT_B = VRAMCNT_B_BG_VRAM_A_OFFS_0K;
+	BG2CNT_A = BGxCNT_EXTENDED_BITMAP_8 | BGxCNT_OVERFLOW_WRAP | BGxCNT_BITMAP_SIZE_256x256 | BGxCNT_BITMAP_BASE_0K;
 	BG2CNT_A = (BG2CNT_A&~BGxCNT_PRIORITY_MASK)|BGxCNT_PRIORITY_1;
-	BG2_XDX = (1 << 10);
+	BG2_XDX = (1 << 8);
 	BG2_XDY = 0;
 	BG2_YDX = 0;
-	BG2_YDY = (1 << 10);
+	BG2_YDY = (1 << 8);
 	BG2_CX = 0;
 	BG2_CY = 0;
 
@@ -94,159 +186,282 @@ void effect3_init() {
 	ribbonSprite[DIR_RIGHT][DIR_DOWN] = loadSprite16A( "nitro:/gfx/ribbon_rightdown.img.bin" );
 
 	// Palette
-	load8bVRAMIndirect("nitro:/gfx/ribbon_rightleft.pal.bin", PALRAM_OBJ_A,256*2);
+	load8bVRAMIndirect("nitro:/gfx/ribbon_rightleft.pal.bin", PALRAM_OBJ_A,256);
 
-	// Base ribbon for testing
-	ribbons[0].tiles[0].x = 8;
-	ribbons[0].tiles[0].y = 0;
-	ribbons[0].tiles[0].from = DIR_UP;
-	ribbons[0].tiles[0].to = DIR_DOWN;
-
-	ribbons[0].tile_count = 1;
-	ribbons[0].x = 8;
-	ribbons[0].y = 0;
-	ribbons[0].head_dir = DIR_DOWN;
-	ribbons[0].head_status = 3;
+	reset_ribbons();
+	reset_rows();
+	
+	irqSet(IRQ_HBLANK,&switchSprites);
+	irqEnable( IRQ_HBLANK );	
 }
 
 void update_ribbons() {
-	int ribbon_cnt = 0;
-	ribbons[ribbon_cnt].head_status = (ribbons[ribbon_cnt].head_status + 1) % 8;
-	if(ribbons[ribbon_cnt].head_status == 0) {
-		int forbidden_dir = -1;
-		int forbidden_dir_2 = -1;
-		if(ribbons[ribbon_cnt].head_dir == DIR_RIGHT) {
-			ribbons[ribbon_cnt].x++;
-			forbidden_dir = DIR_LEFT;
-		}
-		if(ribbons[ribbon_cnt].head_dir == DIR_LEFT) {
-			ribbons[ribbon_cnt].x--;
-			forbidden_dir = DIR_RIGHT;
-		}
-		if(ribbons[ribbon_cnt].head_dir == DIR_DOWN) {
-			ribbons[ribbon_cnt].y++;
-			forbidden_dir = DIR_UP;
-		}
-		if(ribbons[ribbon_cnt].head_dir == DIR_UP) {
-			ribbons[ribbon_cnt].y--;
-			forbidden_dir = DIR_DOWN;
-		}
-		if(ribbons[ribbon_cnt].x < 0) {
-			forbidden_dir_2 = DIR_LEFT;
-		}
-		if(ribbons[ribbon_cnt].x > 15) {
-			forbidden_dir_2 = DIR_RIGHT;
-		}
-		if(ribbons[ribbon_cnt].y < 0) {
-			forbidden_dir_2 = DIR_UP;
-		}
-		if(ribbons[ribbon_cnt].y > 12) {
-			forbidden_dir_2 = DIR_DOWN;
-		}
-		ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count].x = ribbons[ribbon_cnt].x;
-		ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count].y = ribbons[ribbon_cnt].y;
-		ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count].from = forbidden_dir;
-		do {
+	for(int ribbon_cnt = 0; ribbon_cnt < RIBBON_COUNT; ribbon_cnt++) {
+		ribbons[ribbon_cnt].head_status = (ribbons[ribbon_cnt].head_status + 1) % 8;
+		if(ribbons[ribbon_cnt].head_status == 0) {
+			int forbidden_dir = -1;
+			int forbidden_dir_2 = -1;
+			if(ribbons[ribbon_cnt].head_dir == DIR_RIGHT) {
+				ribbons[ribbon_cnt].x++;
+				forbidden_dir = DIR_LEFT;
+			}
+			if(ribbons[ribbon_cnt].head_dir == DIR_LEFT) {
+				ribbons[ribbon_cnt].x--;
+				forbidden_dir = DIR_RIGHT;
+			}
+			if(ribbons[ribbon_cnt].head_dir == DIR_DOWN) {
+				ribbons[ribbon_cnt].y++;
+				forbidden_dir = DIR_UP;
+			}
+			if(ribbons[ribbon_cnt].head_dir == DIR_UP) {
+				ribbons[ribbon_cnt].y--;
+				forbidden_dir = DIR_DOWN;
+			}
+			if(ribbons[ribbon_cnt].x < 0) {
+				forbidden_dir_2 = DIR_LEFT;
+			}
+			if(ribbons[ribbon_cnt].x > 15) {
+				forbidden_dir_2 = DIR_RIGHT;
+			}
+			if(ribbons[ribbon_cnt].y < 0) {
+				forbidden_dir_2 = DIR_UP;
+			}
+			if(ribbons[ribbon_cnt].y > 12) {
+				forbidden_dir_2 = DIR_DOWN;
+			}
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count].x = ribbons[ribbon_cnt].x;
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count].y = ribbons[ribbon_cnt].y;
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count].from = forbidden_dir;
 			do {
-				ribbons[ribbon_cnt].head_dir = Random() % 4;
-			} while(ribbons[ribbon_cnt].head_dir == forbidden_dir);
-			
-		} while(ribbons[ribbon_cnt].head_dir == forbidden_dir_2);
-		ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count].to = ribbons[ribbon_cnt].head_dir;
-		ribbons[ribbon_cnt].tile_count++;		
+				do {
+					ribbons[ribbon_cnt].head_dir = Random() % 4;
+				} while(ribbons[ribbon_cnt].head_dir == forbidden_dir);
+
+			} while(ribbons[ribbon_cnt].head_dir == forbidden_dir_2);
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count].to = ribbons[ribbon_cnt].head_dir;
+			ribbons[ribbon_cnt].tile_count++;
+		}
 	}
 }
 
-u8 effect3_update( u32 t ) {
+void ribbons_to_rowribbons() {
 
-	update_ribbons();
+	reset_rows();
 	
+	// Newest tiles
+	for(int ribbon_cnt = 0; ribbon_cnt < RIBBON_COUNT; ribbon_cnt++) {
+		u16* newestSprite = ribbonSprite[
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from
+		][
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].to
+		];
+		if(ribbons[ribbon_cnt].head_status == 0) {
+			int old_head;
+			if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_RIGHT) {
+				old_head = DIR_LEFT;
+			}
+			if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_LEFT) {
+				old_head = DIR_RIGHT;
+			}
+			if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_DOWN) {
+				old_head = DIR_UP;
+			}
+			if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_UP) {
+				old_head = DIR_DOWN;
+			}
+			newestSprite = ribbonSprite[old_head][old_head];
+		}
+
+		int idx_y = ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].y;
+		if(idx_y < 0 || idx_y > 11) {
+			continue;
+		}
+		row_tiles[idx_y][row_tile_counts[idx_y]].x = ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].x*16;
+		row_tiles[idx_y][row_tile_counts[idx_y]].y = ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].y*16;
+		row_tiles[idx_y][row_tile_counts[idx_y]].sprite = newestSprite;
+		row_tiles[idx_y][row_tile_counts[idx_y]].prio = 0;
+		row_tile_counts[idx_y]++;
+	}
+
+	// Other tiles
+	int set_one = 0;
+	for(int tile_cnt = MAX_SEGMENTS; tile_cnt > 0; tile_cnt--) {
+		set_one = 0;
+		for(int ribbon_cnt = 0; ribbon_cnt < RIBBON_COUNT; ribbon_cnt++) {
+			if(tile_cnt >= ribbons[ribbon_cnt].tile_count) {
+				continue;
+			}
+
+			int idx_y = ribbons[ribbon_cnt].tiles[tile_cnt].y;
+			if(idx_y < 0 || idx_y > 11 || row_tile_counts[idx_y] >= MAX_PERROW) {
+				continue;
+			}
+			set_one = 1;
+			row_tiles[idx_y][row_tile_counts[idx_y]].x = ribbons[ribbon_cnt].tiles[tile_cnt].x*16;
+			row_tiles[idx_y][row_tile_counts[idx_y]].y = ribbons[ribbon_cnt].tiles[tile_cnt].y*16;
+			row_tiles[idx_y][row_tile_counts[idx_y]].sprite = ribbonSprite[
+				ribbons[ribbon_cnt].tiles[tile_cnt].from][ribbons[ribbon_cnt].tiles[tile_cnt].to
+			];
+			row_tiles[idx_y][row_tile_counts[idx_y]].prio = 1;
+			row_tile_counts[idx_y]++;
+		}
+	}
+
+	// Draw heads directly, in TWO DIFFERENT PLACES.
+	// This is for reasons.
 	int sprite_cnt = 0;
-	int ribbon_cnt = 0;
-
-	// Newest
-	u16* newestSprite = ribbonSprite[
-		ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from
-	][
-		ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].to
-	];
-	if(ribbons[ribbon_cnt].head_status == 0) {
-		int old_head;
-		if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_RIGHT) {
-			old_head = DIR_LEFT;
+	for(int ribbon_cnt = 0; ribbon_cnt < RIBBON_COUNT; ribbon_cnt++) {
+		int head_xd = 0;
+		int head_yd = 0;
+		if(ribbons[ribbon_cnt].head_dir == DIR_RIGHT) {
+			head_xd = 2*ribbons[ribbon_cnt].head_status;
 		}
-		if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_LEFT) {
-			old_head = DIR_RIGHT;
+		if(ribbons[ribbon_cnt].head_dir == DIR_LEFT) {
+			head_xd = -2*ribbons[ribbon_cnt].head_status;
 		}
-		if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_DOWN) {
-			old_head = DIR_UP;
+		if(ribbons[ribbon_cnt].head_dir == DIR_DOWN) {
+			head_yd = 2*ribbons[ribbon_cnt].head_status;
 		}
-		if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_UP) {
-			old_head = DIR_DOWN;
+		if(ribbons[ribbon_cnt].head_dir == DIR_UP) {
+			head_yd = -2*ribbons[ribbon_cnt].head_status;
 		}
-		newestSprite = ribbonSprite[old_head][old_head];
-	}
-
-	oamSet(
-		&oamMain, sprite_cnt,
-		ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].x*16,
-		ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].y*16,
-		1, 0,
-		SpriteSize_16x16,
-		SpriteColorFormat_256Color,
-		newestSprite,
-		32, false, false, false, false, false
-	);
-	sprite_cnt++;
-
-	// Head
-	int head_xd = 0;
-	int head_yd = 0;
-	if(ribbons[ribbon_cnt].head_dir == DIR_RIGHT) {
-		head_xd = 2*ribbons[ribbon_cnt].head_status;
-	}
-	if(ribbons[ribbon_cnt].head_dir == DIR_LEFT) {
-		head_xd = -2*ribbons[ribbon_cnt].head_status;
-	}
-	if(ribbons[ribbon_cnt].head_dir == DIR_DOWN) {
-		head_yd = 2*ribbons[ribbon_cnt].head_status;
-	}
-	if(ribbons[ribbon_cnt].head_dir == DIR_UP) {
-		head_yd = -2*ribbons[ribbon_cnt].head_status;
-	}
-	oamSet(
-		&oamMain, sprite_cnt,
-		ribbons[ribbon_cnt].x*16+head_xd,ribbons[ribbon_cnt].y*16+head_yd,
-		ribbons[ribbon_cnt].head_status <= 1 ? 0 : 1, 0,
-		SpriteSize_16x16,
-		SpriteColorFormat_256Color,
-		ribbons[ribbon_cnt].head_status <= 1 ?
-			zeroSprite[ribbons[ribbon_cnt].head_dir] :
-			ribbonSprite[ribbons[ribbon_cnt].head_dir][ribbons[ribbon_cnt].head_dir],
-		32, false, false, false, false, false
-	);
-	sprite_cnt++;
-	
-	// All-but-newest
-	for(int tile_cnt = ribbons[ribbon_cnt].tile_count - 2; tile_cnt >= 0; tile_cnt-- ) {
 		oamSet(
 			&oamMain, sprite_cnt,
-			ribbons[ribbon_cnt].tiles[tile_cnt].x*16,ribbons[ribbon_cnt].tiles[tile_cnt].y*16,
-			1, 0,
+			ribbons[ribbon_cnt].x*16+head_xd,ribbons[ribbon_cnt].y*16+head_yd,
+			ribbons[ribbon_cnt].head_status <= 1 ? 0 : 1, 0,
 			SpriteSize_16x16,
 			SpriteColorFormat_256Color,
-			ribbonSprite[ribbons[ribbon_cnt].tiles[tile_cnt].from][ribbons[ribbon_cnt].tiles[tile_cnt].to],
+			ribbons[ribbon_cnt].head_status <= 1 ?
+				zeroSprite[ribbons[ribbon_cnt].head_dir] :
+				ribbonSprite[ribbons[ribbon_cnt].head_dir][ribbons[ribbon_cnt].head_dir],
+			32, false, false, false, false, false
+		);
+		oamSet(
+			&oamMain, 64+sprite_cnt,
+			ribbons[ribbon_cnt].x*16+head_xd,ribbons[ribbon_cnt].y*16+head_yd,
+			ribbons[ribbon_cnt].head_status <= 1 ? 0 : 1, 0,
+			SpriteSize_16x16,
+			SpriteColorFormat_256Color,
+			ribbons[ribbon_cnt].head_status <= 1 ?
+				zeroSprite[ribbons[ribbon_cnt].head_dir] :
+				ribbonSprite[ribbons[ribbon_cnt].head_dir][ribbons[ribbon_cnt].head_dir],
 			32, false, false, false, false, false
 		);
 		sprite_cnt++;
 	}
+}
 
+void rowribbon_test() {
+	int sprite_cnt = 0;
+	for(int row = 0; row < 12; row++) {
+		for(int i = 0; i < row_tile_counts[row]; i++) {
+			oamSet(
+				&oamMain, RIBBON_COUNT + sprite_cnt++,
+				row_tiles[row][i].x,row_tiles[row][i].y,
+				row_tiles[row][i].prio, 0,
+				SpriteSize_16x16,
+				SpriteColorFormat_256Color,
+				row_tiles[row][i].sprite,
+				32, false, false, false, false, false
+			);
+		}
+	}
 	oamUpdate(&oamMain);
+}
+
+void draw_ribbons_direct() {
+	int sprite_cnt = 0;
+	for(int ribbon_cnt = 0; ribbon_cnt < RIBBON_COUNT; ribbon_cnt++) {
+		// Newest
+		u16* newestSprite = ribbonSprite[
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from
+		][
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].to
+		];
+		if(ribbons[ribbon_cnt].head_status == 0) {
+			int old_head;
+			if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_RIGHT) {
+				old_head = DIR_LEFT;
+			}
+			if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_LEFT) {
+				old_head = DIR_RIGHT;
+			}
+			if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_DOWN) {
+				old_head = DIR_UP;
+			}
+			if(ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].from == DIR_UP) {
+				old_head = DIR_DOWN;
+			}
+			newestSprite = ribbonSprite[old_head][old_head];
+		}
+
+		oamSet(
+			&oamMain, sprite_cnt,
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].x*16,
+			ribbons[ribbon_cnt].tiles[ribbons[ribbon_cnt].tile_count-1].y*16,
+			1, 0,
+			SpriteSize_16x16,
+			SpriteColorFormat_256Color,
+			newestSprite,
+			32, false, false, false, false, false
+		);
+		sprite_cnt++;
+
+		// Head
+		int head_xd = 0;
+		int head_yd = 0;
+		if(ribbons[ribbon_cnt].head_dir == DIR_RIGHT) {
+			head_xd = 2*ribbons[ribbon_cnt].head_status;
+		}
+		if(ribbons[ribbon_cnt].head_dir == DIR_LEFT) {
+			head_xd = -2*ribbons[ribbon_cnt].head_status;
+		}
+		if(ribbons[ribbon_cnt].head_dir == DIR_DOWN) {
+			head_yd = 2*ribbons[ribbon_cnt].head_status;
+		}
+		if(ribbons[ribbon_cnt].head_dir == DIR_UP) {
+			head_yd = -2*ribbons[ribbon_cnt].head_status;
+		}
+		oamSet(
+			&oamMain, sprite_cnt,
+			ribbons[ribbon_cnt].x*16+head_xd,ribbons[ribbon_cnt].y*16+head_yd,
+			ribbons[ribbon_cnt].head_status <= 1 ? 0 : 1, 0,
+			SpriteSize_16x16,
+			SpriteColorFormat_256Color,
+			ribbons[ribbon_cnt].head_status <= 1 ?
+				zeroSprite[ribbons[ribbon_cnt].head_dir] :
+				ribbonSprite[ribbons[ribbon_cnt].head_dir][ribbons[ribbon_cnt].head_dir],
+			32, false, false, false, false, false
+		);
+		sprite_cnt++;
+
+		// All-but-newest
+		for(int tile_cnt = ribbons[ribbon_cnt].tile_count - 2; tile_cnt >= 0; tile_cnt-- ) {
+			oamSet(
+				&oamMain, sprite_cnt,
+				ribbons[ribbon_cnt].tiles[tile_cnt].x*16,ribbons[ribbon_cnt].tiles[tile_cnt].y*16,
+				1, 0,
+				SpriteSize_16x16,
+				SpriteColorFormat_256Color,
+				ribbonSprite[ribbons[ribbon_cnt].tiles[tile_cnt].from][ribbons[ribbon_cnt].tiles[tile_cnt].to],
+				32, false, false, false, false, false
+			);
+			sprite_cnt++;
+		}
+	}
+	
+	oamUpdate(&oamMain);
+}
+
+u8 effect3_update( u32 t ) {
+	
+	update_ribbons();
+	ribbons_to_rowribbons();
 	
 	return( 0 );
 }
 
 
 void effect3_destroy() {
-
+	irqDisable( IRQ_HBLANK );
 }
