a7bef0b855a829214a67729c13b50e07c194d6aa
halcy <lorenzd@gmail.com>
2012-08-11 02:49:10 +0200

    Interruptery.

--- Main.c
+++ Main.c
@@ -26,6 +26,11 @@
 #include "music.h"
 
 volatile uint32_t t;
+volatile uint32_t toff;
+volatile uint32_t scur;
+volatile uint32_t ecur;
+volatile uint32_t blockload;
+
 static void vblank();
 
 extern int tempImage;
@@ -51,26 +56,28 @@ extern int tempImage;
 // 		memset( master_bright, (1<<7) | 0, 2 );
 // 	}
 // }
+uint16_t* master_bright = (uint16_t*)(0x400006C);
 
 void fadeinout(int t, int s, int e) {
-	uint16_t* master_bright = (uint16_t*)(0x400006C);
-	if( t < s+32 ) {
-		int16_t val = (s+32)-t;
-		if(val > 15) {
-			val = 15;
-		}
-		memset( master_bright, (1<<7) | val, 2 );
-	}
-	else if(t < e - 16) {
-		memset( master_bright, (1<<7) | 0, 2 );
-	}
-	else if( t < e ) {
-		int16_t val = 16-(e-t);
-		memset( master_bright, (1<<7) | val, 2 );
-	}
-	else {
-		memset( master_bright, (1<<7) | 15, 2 );
-	}
+// 	if( t < s+32 ) {
+// 		int16_t val = (s+32)-t;
+// 		if(val > 15) {
+// 			val = 15;
+// 		}
+// 		memset( master_bright, (1<<7) | val, 2 );
+// 	}
+// 	else if(t < e - 16) {
+// 		memset( master_bright, (1<<7) | 0, 2 );
+// 	}
+// 	else if( t < e ) {
+// 		int16_t val = 16-(e-t);
+// 		memset( master_bright, (1<<7) | val, 2 );
+// 	}
+// 	else {
+// 		memset( master_bright, (1<<7) | 15, 2 );
+// 	}
+	scur = s;
+	ecur = e;
 }
 
 // EFFECTS:
@@ -118,7 +125,8 @@ int main()
 	#endif
 
 	t = 0;
-
+	blockload = 0;
+	
 	// Main loop
 // 	#define EFFECT_DEBUG
 	#ifdef EFFECT_DEBUG
@@ -148,7 +156,7 @@ int main()
 	mmStart( MOD_WIENERLONG, MM_PLAY_ONCE );
 	
 	int next_effect_init = 0;
-	int toff = t;
+	toff = t;
 	
 	while( 1 ) { // TODO duration
 
@@ -167,43 +175,50 @@ int main()
 // 		}
 		#else
 
-		effect0_update(t-toff);
 		if( t - toff < 2*DUR ) {
 			// LATER TODO: Update intro
 			fadeinout(t-toff,0,2*DUR);
 		}
 		else if(t - toff < 3*DUR) {
 			if(next_effect_init < 1) {
+				fadeinout(t-toff,2*DUR,3*DUR);
+				blockload = 1;			
 				next_effect_init++;
 				// LATER TODO: Destroy intro
 				effect4_init();
+				blockload = 0;
 			}
 			effect4_update(t);
-			fadeinout(t-toff,2*DUR,3*DUR);			
 		}
 		else if(t - toff  < 4*DUR) {
 			if(next_effect_init < 2) {
+				fadeinout(t-toff,3*DUR,4*DUR);				
 				next_effect_init++;
+				blockload = 1;
 				effect4_destroy();
 				effect3_init();
+				blockload = 0;
 			}
 			effect3_update(t);
-			fadeinout(t-toff,3*DUR,4*DUR);
 		}
 		else if(t - toff < 5*DUR) {
 			if(next_effect_init < 3) {
+				fadeinout(t-toff,4*DUR,5*DUR);				
 				next_effect_init++;
+				blockload = 1;				
 				effect3_destroy();
 				metaballs_init();
+				blockload = 0;
 			}
 			metaballs_update(t);
-			fadeinout(t-toff,4*DUR,5*DUR);
 		}
 		else if(t - toff < 6*DUR) {
 			if(next_effect_init < 4) {
 				next_effect_init++;
+				blockload = 1;
 				metaballs_destroy();
 				InitHeartField();
+				blockload = 0;
 			}
 			RunHeartField(t);
 			fadeinout(t-toff,5*DUR,6*DUR);
@@ -211,8 +226,10 @@ int main()
 		else if(t - toff < 8*DUR) {
 			if(next_effect_init < 5) {
 				next_effect_init++;
+				blockload = 1;
 				// No stop heart field.
 				effect2_init();
+				blockload = 0;
 			}
 			effect2_update(t);
 			fadeinout(t-toff,6*DUR,8*DUR);
@@ -220,8 +237,10 @@ int main()
 		else if(t - toff < 9*DUR) {
 			if(next_effect_init < 6) {
 				next_effect_init++;
+				blockload = 1;
 				effect2_destroy();
 				effect6_init();
+				blockload = 0;
 			}
 			effect6_update(t);
 			fadeinout(t-toff,8*DUR,9*DUR);
@@ -229,8 +248,10 @@ int main()
 		else if(t - toff < 10*DUR) {
 			if(next_effect_init < 7) {
 				next_effect_init++;
+				blockload = 1;
 				effect6_destroy();
 				effect1_init();
+				blockload = 0;
 			}
 			effect1_update(t);
 			fadeinout(t-toff,9*DUR,10*DUR);
@@ -238,9 +259,11 @@ int main()
 		else if(t - toff < 12*DUR) {
 			if(next_effect_init < 8) {
 				next_effect_init++;
+				blockload = 1;
 				effect1_destroy();
 				InitField();
-				RunField(t);				
+				RunField(t);
+				blockload = 0;
 			}
 			RunField(t);
 			fadeinout(t-toff,10*DUR,12*DUR);
@@ -248,8 +271,10 @@ int main()
 		else {
 			if(next_effect_init < 9) {
 				next_effect_init++;
+				blockload = 1;
 				StopField();
 				// TODO Outro init
+				blockload = 0;
 			}
 			// TODO run outro
 			fadeinout(t-toff,12*DUR,14*DUR);
@@ -266,6 +291,32 @@ int main()
 	return 0;
 }
 
-static void vblank() { t++; }
+static void vblank() {
+	t++;
+
+	uint32_t t2 = t - toff;
+	
+	if( t2 <= scur || blockload ) {
+		memset( master_bright, (1<<7) | 15, 2 );
+	} else if( t2 < scur+20 ) {
+		int16_t val = (scur+20)-t2;
+		if(val > 15) {
+			val = 15;
+		}
+		memset( master_bright, (1<<7) | val, 2 );
+	}
+	else if(t2 < ecur - 16) {
+		memset( master_bright, (1<<7) | 0, 2 );
+	}
+	else if( t2 < ecur ) {
+		int16_t val = 16-(ecur-t2);
+		memset( master_bright, (1<<7) | val, 2 );
+	}
+	else {
+		memset( master_bright, (1<<7) | 15, 2 );
+	}
+
+	effect0_update(t-toff);
+}
 
 
